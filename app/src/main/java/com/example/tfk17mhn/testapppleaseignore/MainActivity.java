e61544fd9a21a21d88bc73ab72bb88eaafbeaa11
Pastasallad <pastasallad@gmail.com>
2017-05-31 01:06:58 +0200

    Added colors.xml. Removed hardcoded string from activity_main.xml. Added togglebutton.

--- app/src/main/java/com/example/tfk17mhn/testapppleaseignore/MainActivity.java
+++ app/src/main/java/com/example/tfk17mhn/testapppleaseignore/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.tfk17mhn.testapppleaseignore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    ToggleButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (ToggleButton) findViewById(R.id.toggleButton);
+    }
+
+    public void toggle(View view) {
+        if (button.isChecked())
+            textView.setText("Tjeeena!");
+        else
+            textView.setText("Dåå drar vi hem till söööder.");
     }
 }
